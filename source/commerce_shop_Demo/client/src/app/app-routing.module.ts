f511aafb2380d6919510fd668c9c495808b6a22c
aamirWasi <wasi3aamir@gmail.com>
2021-07-02 20:38:56 +0600

    added checkout functionality

--- source/commerce_shop_Demo/client/src/app/app-routing.module.ts
+++ source/commerce_shop_Demo/client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {path:'',component:HomeComponent,data:{breadcrumb:'Home'}},
   {path:'shop',loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule),data:{breadcrumb:'Shop'}},
   {path:'basket',loadChildren:()=>import('./basket/basket.module').then(mod=>mod.BasketModule),data:{breadcrumb:'Basket'}},
+  {path:'checkout',loadChildren:()=>import('./checkout/checkout.module').then(mod=>mod.CheckoutModule),data:{breadcrumb:'Checkout'}},
   {path:'**',redirectTo:'',pathMatch:'full'}
 ];
 
